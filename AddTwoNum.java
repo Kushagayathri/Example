75fa9f2a6f3e5eb4137414d7ced1aaca7aecefae
Kushagayathri <91619726+Kushagayathri@users.noreply.github.com>
2021-12-02 10:49:17 +0530

    Add files via upload

---
+++ AddTwoNum.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author student
+ */
+public class AddTwoNum {
+    
+    public static void main(String args[])
+    {
+        int x=10,y=15,c;
+        System.out.println("The value of x is:"+x);
+        System.out.println("The value of y is:"+y);
+        c=x+y;
+        System.out.println("The addition of two numbers is:"+c);
+        
+    }
+            
+}
